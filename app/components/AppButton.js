268b660ed9838c4a32f15c381ee980f2f5ee7a95
SyedJawadRaza-Baquar <jawad.raza72021@gmail.com>
2023-12-03 01:27:02 -0700

    added login and registration

---
+++ app/components/AppButton.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { StyleSheet, Text, TouchableOpacity } from "react-native";
+
+import colors from "../config/colors";
+
+function AppButton({ title, onPress, color = "primaryBlue" }) {
+  return (
+    <TouchableOpacity
+      style={[styles.button, { backgroundColor: colors[color] }]}
+      onPress={onPress}
+    >
+      <Text style={styles.text}>{title}</Text>
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: colors.primaryBlue,
+    borderRadius: 25,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 15,
+    width: "100%",
+    marginVertical: 10,
+  },
+  text: {
+    color: colors.softWhite,
+    fontSize: 18,
+    textTransform: "uppercase",
+    fontWeight: "bold",
+  },
+});
+
+export default AppButton;
